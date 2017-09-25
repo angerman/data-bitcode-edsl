5a842d9c2e853ebf2ca13230d81203581fd726eb
Moritz Angermann <moritz.angermann@gmail.com>
2017-09-25 09:42:16 +0800

    Cleanup warnings.

--- src/EDSL/Example/HelloWorld.hs
+++ src/EDSL/Example/HelloWorld.hs
@@ -1,25 +1,25 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults -fno-warn-unused-matches #-}
 
 module EDSL.Example.HelloWorld where
 import Prelude hiding (mod)
 import EDSL
-import EDSL.Monad.Types
-import EDSL.Monad.Instructions
-import EDSL.Monad.Instructions.Binary
+import Data.BitCode.LLVM (Module)
 
 import Data.BitCode.LLVM.Value (external)
 
+helloWorld :: Module
 helloWorld = mod "helloWorld"
   [ def "square" ([i32] --> i32) $ \[ arg0 ] -> do
       block "entry" $ do
         ret =<< arg0 `mul` arg0
   , def "main" ([i32, ptr =<< i8ptr] --> i32) $ \[ argc, argv ] -> do
       block "entry" $ do
-        foo <- global external "foo" =<< (cStr "hello world, %d\n") 
+        foo <- global external "foo" =<< (cStr "hello world, %d\n")
         strPtr  <- gep foo =<< sequence [int32 0, int32 0]
-        square <- fun "square" =<< [i32] --> i32 
+        square <- fun "square" =<< [i32] --> i32
         Just sq <- ccall square =<< sequence [int32 3]
-        f <- fun "printf" =<< (vararg $ [i8ptr] --> i32)
-        ccall f [strPtr, sq]
+        printf <- fun "printf" =<< (vararg $ [i8ptr] --> i32)
+        ccall printf [strPtr, sq]
         ret =<< int32 0
   ]
